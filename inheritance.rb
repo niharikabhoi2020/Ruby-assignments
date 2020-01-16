a443d25827e00147685f257ca6b598e7278d3232
niharikabhoi2020 <niharikabhoi2016@gmail.com>
2020-01-16 15:47:00 +0530

    Changes made as per ruby styles

--- inheritance.rb
+++ inheritance.rb
@@ -1,42 +1,46 @@
 class Person
-  @@hash  ={"Employee"  => "20K", "Senior_person" =>  "40K", "Customer" => "None"}
+  @@hash = {
+  "Employee" => "20K", 
+  "Senior_person" => "40K", 
+  "Customer" => "None" }
 
-  def initialize(name,post)
-    @name=name
-    @post=post
+  def initialize(name, post)
+    @name = name
+    @post = post
   end
 
+  
   protected 
   def protected_method
-    puts "salary of #{@name} is #{@@hash[@post]}"
+    puts "salary of #{ @name } is #{ @@hash[@post] }"
   end
 end
 
 class Employee < Person
-  def salary_eml
+  def salary_of_employee
     protected_method
   end
 end
 
 class Senior_person < Person
-  def salary_snrp
+  def salary_of_senior_person
     protected_method
   end
 end
 
 class Customer < Person
-  def salary_cstp
+  def salary_of_customer
     protected_method
   end
 end
 
 
-emply = Employee.new("Niharika","Employee")
-emply.salary_eml
+emply = Employee.new("Niharika", "Employee")
+emply.salary_of_employee
 
-snr = Senior_person.new("Tejas","Senior_person")
-snr.salary_snrp
+snr = Senior_person.new("Tejas", "Senior_person")
+snr.salary_of_senior_person
 
-cst = Customer.new("Sagar","Customer")
-cst.salary_cstp
+cst = Customer.new("Sagar", "Customer")
+cst.salary_of_customer
 
