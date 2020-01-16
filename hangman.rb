73f9d840c1c018897aa41cb46d45aa8c88ac915a
niharikabhoi2020 <niharikabhoi2016@gmail.com>
2020-01-16 15:39:55 +0530

    changes made as per Ruby styles

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,15 @@
-possible_words = ["sagar","niharika","tejas","akansha","deepali","mayur","latikesj","abhishek","anurag","shubham"]
+possible_words = ["sagar", "niharika", "tejas", "akansha", "deepali", "mayur", "latikesj", "abhishek", "anurag", "shubham"]
 
-word  = possible_words.sample
+word = possible_words.sample
 word_length = word.length
 
-hash  = Hash.new{|h,k| h[k] = [] }
+hash = Hash.new{ |h,k| h[k] = [] }
 selected_word = word.split('')
 
-selected_word.each_with_index { |value,index| [hash[value].push(index)]  }
+selected_word.each_with_index { |value,index| [hash[value].push(index)] }
 
 hash_size = hash.size
-display=Array.new
+display = Array.new
 
 for i in 0...word_length
   display.push("_")
@@ -18,28 +18,28 @@ end
 puts "Guess the word"
 p display
 
-attempts  = 5
-correct_letter  = hash_size
-puts "You have #{attempts} attempts"
+attempts = 5
+correct_letter = hash_size
+puts "You have #{ attempts } attempts"
  
-while correct_letter>0  
+while correct_letter > 0  
 
-  if attempts>0
+  if attempts > 0
     flag = false
     puts "Enter an alphabet"
     alphabet = gets.chomp
 
     hash.each do |key, value|
-      if key==alphabet
-        flag  =  true
+      if key == alphabet
+        flag =  true
         position = value
-        position.each { |x| display[x]= alphabet}
+        position.each { |x| display[x]= alphabet }
         p display
         correct_letter -=  1
       end
     end
 
-    if correct_letter==0
+    if correct_letter == 0
       puts "You won"
       p display
     end
@@ -52,7 +52,7 @@ while correct_letter>0
         p  selected_word
         correct_letter = 0
       else
-        puts "You have #{attempts} attempts"
+        puts "You have #{ attempts } attempts"
       end
     end
 
